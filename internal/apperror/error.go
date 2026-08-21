74aad10a0ebe273354b6ea94d677c8d326cfd51d
Petr Kuchinov <petr.kuchinov@axxonsoft.dev>
2026-08-21 12:25:51 +0300

    Added output domain errors

--- internal/apperror/error.go
+++ internal/apperror/error.go
@@ -3,6 +3,17 @@ package apperror
 import "errors"
 
 var (
-	ErrCodeAlreadyExists = errors.New("custom code is already taken")
-	ErrInvalidCustomCode = errors.New("custom code contains invalid characters")
+	ErrNotFound           = errors.New("short link not found")
+	ErrCodeAlreadyExists  = errors.New("custom code already exists")
+	ErrInvalidCustomCode  = errors.New("invalid custom code format")
+	ErrInternal           = errors.New("internal server error")
 )
+
+type AppError struct {
+	Err    error
+	Message string
+}
+
+func (e *AppError) Error() string {
+	return e.Message
+}
